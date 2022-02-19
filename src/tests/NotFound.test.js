34b7f70eb41d64e419521a15d0d026a12f125443
¨Venturin¨ <diegoventurinn@gmail.com>
2022-02-19 16:52:15 -0300

    Implementado o requisito 04

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('Teste o componente <NotFound.js />.',
+  () => {
+    test('Teste se página contém um heading h2 com o texto correto', () => {
+      render(<NotFound />);
+
+      const h2 = screen.getByRole(
+        'heading', { name: 'Page requested not found Crying emoji', level: 2 },
+      );
+      expect(h2).toBeInTheDocument();
+    });
+
+    test('Testa se a página contém a imagem com a URL correta', () => {
+      render(<NotFound />);
+      const url = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+      const img = screen.getByAltText(
+        'Pikachu crying because the page requested was not found',
+      );
+      expect(img.src).toContain(url);
+    });
+  });
